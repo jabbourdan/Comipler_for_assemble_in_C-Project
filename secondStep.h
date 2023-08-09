d8d14f9f1809c777aa307367bb9c9a2aaf2f7397
Jabbour Dandan <111487226+jabbourdan@users.noreply.github.com>
2023-08-09 16:25:49 +0300

    Create secondStep.h

---
+++ secondStep.h
@@ -0,0 +1,12 @@
+#ifndef SECOND_STEP_H
+#define SECOND_STEP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "firstStep.h"
+#include "globals.h"
+
+void secondCheck(char* fileName,struct  dataTable* stail, struct  machineCode* dtail,int *IC,int *DC);
+#endif /* MAKE_AM_H */
